5abcade360a335d25c086d970eb2a78a33b50424
Gaston Gutierrez <gastongutierrez964@gmail.com>
2024-03-13 00:34:51 -0300

    Update 2

--- src/Components/Mapa.jsx
+++ src/Components/Mapa.jsx
@@ -2,10 +2,15 @@
 import React from 'react';
 
 const Mapa = () => {
+    const handlePdfClick = () => {
+        // Redirige a la URL del PDF
+        window.open('\Área Productos Biomédicos.pdf', '_blank');
+    };
+
     return (
-        <div>
-            <h2>Mapa Page</h2>
-            {/* Contenido del mapa */}
+        <div className='mapa'>
+            <h2>Mapa Productos Biomedicos</h2>
+            <button className='button-59' onClick={handlePdfClick}>Click para ver el Mapa</button>
         </div>
     );
 };
